499f4675b75ca41652e9ab9ed8f35c7d5938f499
biccsdev <biccsdev@gmail.com>
2023-10-29 20:00:27 -0600

    added tweeting implementation

--- scrape.js
+++ scrape.js
@@ -32,51 +32,19 @@ getData()
     const { status, scoreVisitor, scoreHome } = data;
 
     console.log(status);
+    console.log(scoreVisitor);
+    console.log(scoreHome);
 
-    if (scoreVisitor < scoreHome) {
-      console.log('SI');
+    if (status == 'FINALIZADO') {
+      if (scoreVisitor < scoreHome) {
+        console.log('SI');
+      } else {
+        console.log('NO');
+      }
     } else {
-      console.log('NO');
+      console.log('El juego no ha terminado')
     }
   })
   .catch((error) => {
     console.error(error);
   });
-
-
-// import https from "https"
-
-// const url = 'https://cañeros.net'; // URL of the webpage to scrape
-// let status = ''; // Must be "FINALIZADO"
-// let scoreVisitor = ''; // Score for the visitor team
-// let scoreHome = ''; // Score for the local team
-
-// async function getData() {
-//   https.get(url, (response) => {
-//     let data = '';
-
-//     response.on('data', (chunk) => {
-//       data += chunk;
-//     });
-
-//     response.on('end', () => {
-//       status = data.substring(4488, 4498);
-//       scoreVisitor = data.substring(4855, 4856);
-//       scoreHome = data.substring(5247, 5248);
-//     });
-//   }).on('error', (error) => {
-//     console.error(`Error: ${error.message}`);
-//   });
-// }
-
-// await getData();
-
-// console.log(status)
-
-// if (scoreVisitor < scoreHome) {
-//   console.log('SI');
-// } else {
-//   console.log('NO');
-// }
-
-
